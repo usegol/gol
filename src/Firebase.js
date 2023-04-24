fe53544452d8e76f220ed0b46ffe19d9fee31590
Elvin Atwine <elivnatwiine@gmail.com>
2023-04-24 07:08:11 -0700

    env

--- src/Firebase.js
+++ src/Firebase.js
@@ -2,19 +2,16 @@ import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDRPxqaxFENphq6i3yfKDBzj7FxTYKxzKM",
-  authDomain: "use-gol.firebaseapp.com",
-  projectId: "use-gol",
-  storageBucket: "use-gol.appspot.com",
-  messagingSenderId: "130570097756",
-  appId: "1:130570097756:web:85f79e01d110bf7fc20299",
-  measurementId: "G-VZMCRQT53W"
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID
 };
 
 // Initialize Firebase
