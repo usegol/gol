e45795503e512e3f1e0c9412afd84aeed0fd50dd
Elvin Atwine <elivnatwiine@gmail.com>
2023-04-27 17:04:25 -0700

    goal details nav

--- src/Dashboard/GoalDetail.js
+++ src/Dashboard/GoalDetail.js
@@ -1,13 +1,17 @@
 import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
-import { doc, getDoc, collection, query, where, onSnapshot, addDoc, updateDoc } from 'firebase/firestore';
+import { doc, getDoc, collection, query, where, onSnapshot, addDoc, updateDoc, deleteDoc } from 'firebase/firestore';
 import { db } from '../Firebase';
+import { FiTrash2 } from 'react-icons/fi';
+import { useNavigate } from 'react-router-dom';
 
 const GoalDetail = () => {
   const { goalId } = useParams();
   const [goal, setGoal] = useState(null);
   const [todos, setTodos] = useState([]);
   const [newTodo, setNewTodo] = useState('');
+
+    const navigate = useNavigate();
 
   useEffect(() => {
     const fetchGoal = async () => {
@@ -41,9 +45,6 @@ const GoalDetail = () => {
       fetchTodos();
     }
   }, [goalId, todos]);
-  
-  
-  
 
   const addTodo = async () => {
     if (newTodo.trim()) {
@@ -56,7 +57,6 @@ const GoalDetail = () => {
       setNewTodo('');
     }
   };
-  
 
   const toggleTodoCompletion = async (todoId, completed) => {
     await updateDoc(doc(db, 'todos', todoId), { completed: !completed });
@@ -66,6 +66,10 @@ const GoalDetail = () => {
     if (newTitle.trim()) {
       await updateDoc(doc(db, 'todos', todoId), { title: newTitle });
     }
+  };
+
+  const deleteTodo = async (todoId) => {
+    await deleteDoc(doc(db, 'todos', todoId));
   };
 
   const calculateDaysRemaining = (deadline) => {
@@ -87,8 +91,15 @@ const GoalDetail = () => {
         <h1 className="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900">{goal.title}</h1>
         <p className="text-center text-sm text-gray-600">{goal.description}</p>
         <p className="text-center text-sm font-bold text-gray-600">Days remaining: {daysRemaining}</p>
+        <div className='flex justify-center mt-4'>
+            <button
+                    onClick={() => navigate('/dashboard')}
+                    className="mt-4 bg-gray-200 text-gray-800 text-sm font-semibold px-4 py-2 rounded-md hover:bg-gray-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500"
+                    >
+                    Back to Dashboard
+            </button>
+        </div>
       </div>
-
       <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
         <div className="bg-white px-4 py-8 shadow sm:rounded-lg sm:px-10">
           <h2 className="text-center text-xl font-bold tracking-tight text-gray-900">To-Do List</h2>
@@ -107,6 +118,9 @@ const GoalDetail = () => {
                   onBlur={(e) => editTodo(todo.id, e.target.value)}
                   className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-green-600 sm:text-sm sm:leading-6"
                 />
+                <button onClick={() => deleteTodo(todo.id)} className="text-red-500 focus:outline-none">
+                  <FiTrash2 size={18} />
+                </button>
               </li>
             ))}
           </ul>
@@ -123,8 +137,6 @@ const GoalDetail = () => {
         </div>
       </div>
     </div>
-
-
   );
 };
 
